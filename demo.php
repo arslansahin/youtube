54c77cd258bb0b83404cdef01effe27bd77adc94
Arslan Şahin <arsann@gmail.com>
2017-07-06 10:11:16 +0300

    Create demo.php

---
+++ demo.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once 'youtube.php';
+
+//youtube video url ile
+$data = 'https://www.youtube.com/watch?v=Hu97Hk8MT8U';
+
+$youtube = new youtube($data);
+$response = array(
+  'title'=>$youtube->title(),
+  'desc'=>$youtube->description(),
+  'img'=>$youtube->image(),
+);
+
+print_r($response);
+
+
+//youtube video embed kodu ile
+$data = '<iframe width="560" height="315" src="https://www.youtube.com/embed/Hu97Hk8MT8U" frameborder="0" allowfullscreen></iframe>';
+
+$youtube = new youtube($data);
+$response = array(
+  'title'=>$youtube->title(),
+  'desc'=>$youtube->description(),
+  'img'=>$youtube->image(),
+);
+
+print_r($response);
+
+?>
